a3a3b1617d4c2c0ef9cb8294cde949c84c795ba0
DavidZhong <dongfan.zhong@gmail.com>
2016-07-04 23:33:54 -0700

    change the password

--- samples/db/do_sqlalchemy.py
+++ samples/db/do_sqlalchemy.py
@@ -18,14 +18,14 @@ class User(Base):
     name = Column(String(20))
 
 # 初始化数据库连接:
-engine = create_engine('mysql+mysqlconnector://root:password@localhost:3306/test')
+engine = create_engine('mysql+mysqlconnector://root:welcome2WD@localhost:3306/test')
 # 创建DBSession类型:
 DBSession = sessionmaker(bind=engine)
 
 # 创建session对象:
 session = DBSession()
 # 创建新User对象:
-new_user = User(id='5', name='Bob')
+new_user = User(id='10', name='Mike')
 # 添加到session:
 session.add(new_user)
 # 提交即保存到数据库:
@@ -36,7 +36,7 @@ session.close()
 # 创建Session:
 session = DBSession()
 # 创建Query查询，filter是where条件，最后调用one()返回唯一行，如果调用all()则返回所有行:
-user = session.query(User).filter(User.id=='5').one()
+user = session.query(User).filter(User.id=='10').one()
 # 打印类型和对象的name属性:
 print('type:', type(user))
 print('name:', user.name)
